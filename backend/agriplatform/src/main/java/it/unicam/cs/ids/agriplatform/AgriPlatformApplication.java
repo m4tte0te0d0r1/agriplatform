b331cdb8b1db57de59a24ef10d3dd621714f2192
matteo <matteo.teodori@dynacode.it>
2024-12-08 17:36:52 +0100

    init project

---
+++ backend/agriplatform/src/main/java/it/unicam/cs/ids/agriplatform/AgriPlatformApplication.java
@@ -0,0 +1,12 @@
+package it.unicam.cs.ids.agriplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgriPlatformApplication.class, args);
+    }
+}
+
